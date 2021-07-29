e4df3fc013c82a6b76c3dc19ded1d04aff4bc41f
HongJunMo <wnsah717@daum.net>
2021-07-29 13:54:31 +0900

    quiz 02, 03 풀이중첩 반복문

--- src/day05_loop/Ex04.java
+++ src/day05_loop/Ex04.java
@@ -4,15 +4,12 @@ public class Ex04 {
 	public static void main(String[] args) {
 
 		int su = 1, sum = 0;
-		for (; su <= 10;) {
+		for (su=1; su <= 10;) {
 			sum += su;
 			su++;
 		}
 		System.out.println("1~10ÇÕ : " + sum);
 
-		for (;;) {
-			System.out.println("½ÇÇà");
-		}
 
 	}
 }
---
+++ src/day05_loop/Quiz02.java
@@ -0,0 +1,25 @@
+package day05_loop;
+
+public class Quiz02 {
+	public static void main(String[] args) {
+
+		for (int i =0; i < 5; i++) {
+
+			for (int j = 1; j < 6; j++) {
+				System.out.print(j + (5*i)+" ");
+
+				}
+			System.out.println();
+			}
+
+
+//		for (int i = 1; i < 6; i++) {
+//
+//			for (int j = 1; j <6; j++) {
+//				System.out.print(i*j+" ");
+//			}
+//			System.out.println();
+//		}
+
+	}
+}
